c6ac6078e3d04b8feb5a89a81d03a9699bbb2e16
huhinchang <83922479+huhinchang@users.noreply.github.com>
2023-04-01 10:11:33 -0400

    Q and E keys control rotation as well

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -17,5 +17,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A)) transform.position += Vector3.left;
         if (Input.GetKeyDown(KeyCode.S)) transform.position += Vector3.back;
         if (Input.GetKeyDown(KeyCode.D)) transform.position += Vector3.right;
+        if (Input.GetKeyDown(KeyCode.Q)) transform.Rotate(0, -90, 0);
+        if (Input.GetKeyDown(KeyCode.E)) transform.Rotate(0, 90, 0);
     }
 }
