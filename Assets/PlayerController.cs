623db13a4ce658d467222fd478f53530848be64c
sunny-lan <sunny.lan@uwaterloo.ca>
2023-04-01 23:50:54 -0400

    Add outline asset

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -43,45 +43,52 @@ public class PlayerController : GridEntity
         return best;
     }
 
+    void TryMove(Vector2Int newPosition)
+    {
+        if (gameManager.IsWalkable(newPosition))
+        {
+            pos = newPosition;
+
+            transform.position = new(pos.x, 0, pos.y);
+            OnDidMove();
+        }
+    }
+
+    void OnDidMove()
+    {
+        moveCnt++;
+        if (moveCnt % 2 == 0)
+        {
+            gameManager.DoEnemyTurn();
+        }
+    }
+
     // Update is called once per frame
     protected override void Update()
     {
         base.Update();
 
+
+
         Vector2Int facing = GetFacingDir();
 
-        bool didMove = false;
-        Vector2Int newPosition = pos;
-
         // Check inputs
-        if (Input.GetKeyDown(KeyCode.W)) { newPosition += facing; }
-        if (Input.GetKeyDown(KeyCode.A)) { newPosition += facing.Left();}
-        if (Input.GetKeyDown(KeyCode.S)) { newPosition += facing.Back();}
-        if (Input.GetKeyDown(KeyCode.D)) { newPosition += facing.Right();}
-        //if (Input.GetKeyDown(KeyCode.Q)) { transform.Rotate(0, -90, 0);}
-        //if (Input.GetKeyDown(KeyCode.E)) { transform.Rotate(0, 90, 0);}
+        if (Input.GetKeyDown(KeyCode.W))  TryMove( pos + facing); 
+        else if (Input.GetKeyDown(KeyCode.A))  TryMove( pos + facing.Left());
+        else if (Input.GetKeyDown(KeyCode.S))  TryMove( pos + facing.Back());
+        else if (Input.GetKeyDown(KeyCode.D))  TryMove( pos + facing.Right());
 
-        // Check valid
-        if (newPosition != pos)
-        {
-            if (gameManager.IsWalkable(newPosition))
-            {
-                pos = newPosition;
 
-                transform.position = new(pos.x, 0, pos.y);
-                didMove = true;
-            }
-        }
+    }
 
-        // Call DoEnemyTurn
-        if (didMove)
-        {
-            moveCnt++;
-            if (moveCnt % 2 == 0)
-            {
-                gameManager.DoEnemyTurn();
-            }
-        }
+    public int bitesToHuman = 3;
+
+    int needToBite;
+
+    public void GetBitten()
+    {
+        needToBite = 3;
+
     }
 
 }
