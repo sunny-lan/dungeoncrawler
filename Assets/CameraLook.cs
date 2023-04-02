2d0d86e2fe139802ade581061ce49f1281759eb6
huhinchang <83922479+huhinchang@users.noreply.github.com>
2023-04-02 11:13:46 -0400

    Camera can't turn upside down now

--- Assets/CameraLook.cs
+++ Assets/CameraLook.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
-    Vector2 rotation = Vector2.zero;
-    public float speed = 3;
+    [SerializeField] float mouseSensitivity = 3f;
+    [SerializeField] Transform playerBody = null;
+    [SerializeField] Transform camera = null;
+    [SerializeField] bool invertY = true;
+    float pitch = 0f;
 
     private void Start()
     {
@@ -19,8 +22,12 @@ public class CameraLook : MonoBehaviour
             Cursor.lockState = Cursor.lockState != CursorLockMode.Locked ? CursorLockMode.Locked : CursorLockMode.None;
         }
 
-        rotation.y += Input.GetAxis("Mouse X");
-        rotation.x += -Input.GetAxis("Mouse Y");
-        transform.eulerAngles = (Vector2)rotation * speed;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        pitch += (invertY)? -mouseY:mouseY;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        camera.localRotation = Quaternion.Euler(Vector3.right * pitch);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
