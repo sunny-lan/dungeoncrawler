790ded05258f2e3cdbffe87e4963f454bddf52bb
sunny-lan <sunny.lan@uwaterloo.ca>
2023-04-02 02:30:54 -0400

    Add HP bar

---
+++ Assets/HPBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+    // Value between -1 and 1
+    public float HP;
+    private float _hpLerp;
+
+    [SerializeField] RectTransform negative, positive;
+
+    public float lerpSpeed = 1;
+    public float minWidth = 0.02f;
+
+    private void Update()
+    {
+        _hpLerp = Mathf.Lerp(_hpLerp, HP, Time.deltaTime * lerpSpeed);
+
+        var scale = positive.localScale;
+        scale.x = Mathf.Max(minWidth, _hpLerp );
+        positive.localScale = scale;
+
+        scale = negative.localScale;
+        scale.x = Mathf.Max(minWidth, -_hpLerp );
+        negative.localScale = scale;
+    }
+}
