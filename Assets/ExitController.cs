12a6de5a09177b71d15c7f56402e6427fdb50c79
huhinchang <83922479+huhinchang@users.noreply.github.com>
2023-04-06 21:25:20 -0400

    Exit mechanic
    Need to decide what actually happens on exit

---
+++ Assets/ExitController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ExitController : MonoBehaviour
+{
+    [SerializeField] Material openMat;
+    [SerializeField] Material closedMat;
+    [SerializeField] new Renderer renderer;
+    [SerializeField] TextMeshPro text;
+    [SerializeField] Color openColor;
+    [SerializeField] Color closedColor;
+
+    private GameManager gameManager;
+    bool locked = true;
+
+    private void Start()
+    {
+        renderer.material = closedMat;
+        text.color = closedColor;
+        gameManager = GameManager.Instance;
+        gameManager.player.onChangePos.AddListener(OnPlayerChangePos);
+    }
+
+    public void UpdateKeyCount(int collected, int total)
+    {
+        text.text = $"EXIT LOCKED\n{collected}/{total} keys found";
+    }
+
+    public void Unlock()
+    {
+        locked = false;
+        text.text = $"EXIT UNLOCKED";
+        renderer.material = openMat;
+        text.color = openColor;
+    }
+
+    private void OnPlayerChangePos(Vector2Int playerPos)
+    {
+        var myPos = new Vector2Int(Mathf.FloorToInt(transform.position.x), Mathf.FloorToInt(transform.position.z));
+        if (myPos == playerPos && !locked)
+        {
+            Debug.Log("LEVEL COMPLETE");
+        }
+    }
+}
